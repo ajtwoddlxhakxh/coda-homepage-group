904821eb3e28002ac0f57bdb4ca3bbacb7d11e91
osori <a01024784424@gmail.com>
2025-08-17 00:27:51 +0900

    Sub_Project 구현완료

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,7 @@
+import SubProject from "./sub_project";
+
 function App() {
-  return (
-    <div>
-      <h1>My App</h1>
-      <p>React 프로젝트 시작!</p>
-    </div>
-  );
+  return <SubProject />;
 }
 
 export default App;
