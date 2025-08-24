c94c3dfb468a58bebb8fb32d141f88d5dcd81dfa
osori <a01024784424@gmail.com>
2025-08-24 18:05:02 +0900

    Sub_main 구현완

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
 import SubProject from "./sub_project";
 import CreateBy from "./CreateBy";
 import Navbar from "./Navbar";
+import Sub_main from "./Sub_main";
 
 function App() {
-    const developer = [
-        { name: '유하선', part: 'BACK-END' },
-        { name: '유도현', part: 'BACK-END' },
-        { name: '권영은', part: 'FRONT-END' },
-        { name: '남태현', part: 'FRONT-END' },
-    ]; /*Create By 명단*/
+  const developer = [
+    { name: "유하선", part: "BACK-END" },
+    { name: "유도현", part: "BACK-END" },
+    { name: "권영은", part: "FRONT-END" },
+    { name: "남태현", part: "FRONT-END" },
+  ]; /*Create By 명단*/
   return (
-      <div className="App">
-          <CreateBy items={developer} specialName={'leader'}/>
+    <div className="App">
+      <CreateBy items={developer} specialName={"leader"} />
 
-          <SubProject />
-          <Navbar />
-      </div>
+      <SubProject />
+      <Sub_main />
 
-      );
+      {/* Navbar 컴포넌트 추가 */}
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- my-app/src/sub_project.js
+++ my-app/src/sub_project.js
@@ -7,8 +7,8 @@ export default function SubProject() {
       {/* study 영역 */}
       <div className="qm-item qm-pos-study qm-glow-study">
         <div className="qm-icon-wrap">
-          <img src="/img/gift.svg" alt="Gift" className="qm-icon" />
-          <img src="/img/cursor.svg" alt="Cursor" className="qm-cursor" />
+          <img src="/images/gift.svg" alt="Gift" className="qm-icon" />
+          <img src="/images/cursor.svg" alt="Cursor" className="qm-cursor" />
         </div>
         <span className="qm-label">STUDY</span>
       </div>
@@ -17,12 +17,12 @@ export default function SubProject() {
       <div className="qm-item qm-pos-award qm-glow-award qm-right">
         <div className="qm-icon-wrap">
           <img
-            src="/img/crown.svg"
+            src="/images/crown.svg"
             alt="Crown"
             className="qm-icon qm-icon--award"
           />
           <img
-            src="/img/cursor.svg"
+            src="/images/cursor.svg"
             alt="Cursor"
             className="qm-cursor qm--award"
           />
@@ -33,8 +33,8 @@ export default function SubProject() {
       {/* activity 영역 */}
       <div className="qm-item qm-pos-activity qm-glow-activity">
         <div className="qm-icon-wrap">
-          <img src="/img/gamepad.svg" alt="Gamepad" className="qm-icon" />
-          <img src="/img/cursor.svg" alt="Cursor" className="qm-cursor" />
+          <img src="/images/gamepad.svg" alt="Gamepad" className="qm-icon" />
+          <img src="/images/cursor.svg" alt="Cursor" className="qm-cursor" />
         </div>
         <span className="qm-label">Activity</span>
       </div>
