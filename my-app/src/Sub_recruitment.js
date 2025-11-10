e7d3d1f3123c0284dea958686598cce5c0ad0488
osori <a01024784424@gmail.com>
2025-11-10 18:18:26 +0900

    recruit 수정

--- my-app/src/Sub_recruitment.js
+++ my-app/src/Sub_recruitment.js
@@ -19,6 +19,19 @@ export default function Sub_recruitment () {
           <span className="init init-CHANCE">'CHANCE'</span>이다.
         </div>
         </div>
+
+      <div className = "recru-sub-title">
+        <span className = "sub-recru-title">BEFORE SIGN UP</span>
+      </div>
+
+        <div className = "recru-sub">
+          <div className = "recru-rule">
+            <span className = "rule-1"> 규칙 1 블로그 업로드 및 깃허브 커밋</span>
+            <span className = "rule-2"> 규칙 2 매달 1회 개인 참여 사업 및 프로젝트 조사</span>
+            <span className = "rule-3"> 규칙 3 열심히 노력하는 모습을 보여줄 것.</span>
+          </div>
+        </div>
+        
         <img className ="ballongift" src="/images/ballongift.svg" alt="ballongift" />
         <img className="rocket" src="/images/rocket.svg" alt="rocket" />
     </div>
